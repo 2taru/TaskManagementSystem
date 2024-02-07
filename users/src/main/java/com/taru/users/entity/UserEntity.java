3c4feb5b862dc1969b0d9cc3dcfc59539fc7faa6
Timur Turianytsia <turianytsia.timur@gmail.com>
2024-02-07 16:40:57 +0200

    feat: Added Base and User Entities

---
+++ users/src/main/java/com/taru/users/entity/UserEntity.java
@@ -0,0 +1,19 @@
+package com.taru.users.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter @ToString
+public class UserEntity extends  BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String username;
+
+    private String email;
+}
