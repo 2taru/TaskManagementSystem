a1df162b6e3235b732fda907860b0cd5782ee8ee
Timur Turianytsia <turianytsia.timur@gmail.com>
2024-02-08 10:16:53 +0200

    feat: added repository and DTO layers

---
+++ users/src/main/java/com/taru/users/dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.taru.users.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private String username;
+
+    private String email;
+}
---
+++ users/src/main/java/com/taru/users/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.taru.users.repository;
+
+import com.taru.users.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+}
