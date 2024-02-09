a3adbee4b4bd49c45987e33979d098849386e474
Timur Turianytsia <turianytsia.timur@gmail.com>
2024-02-09 21:28:35 +0200

    feat: added swagger documentation

--- tasks/src/main/java/com/taru/tasks/controller/TaskController.java
+++ tasks/src/main/java/com/taru/tasks/controller/TaskController.java
@@ -1,8 +1,15 @@
 package com.taru.tasks.controller;
 
+import com.taru.tasks.dto.ErrorResponseDTO;
 import com.taru.tasks.dto.ResponseDTO;
 import com.taru.tasks.dto.TaskDTO;
 import com.taru.tasks.service.TaskService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,6 +20,10 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(
+        name = "CRUD REST APIs for Task in TaskManager",
+        description = "CRUD REST APIs in TaskManager to CREATE, UPDATE, GET AND DELETE tasks"
+)
 @RestController
 @Validated
 @AllArgsConstructor
@@ -21,6 +32,30 @@ public class TaskController {
 
     private final TaskService taskService;
 
+    @Operation(
+            summary = "Create Task REST API",
+            description = "REST API to create new Tasks inside TaskManager"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "HTTP Status BAD_REQUEST",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP INTERNAL_SERVER_ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    })
     @PostMapping("/create")
     public ResponseEntity<ResponseDTO> createTask(@Valid @RequestBody TaskDTO taskDTO) {
 
@@ -31,6 +66,30 @@ public class TaskController {
                 .body(new ResponseDTO("201", "Task created successfully!"));
     }
 
+    @Operation(
+            summary = "Get Task Details REST API",
+            description = "REST API to get task information based on taskId"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP Status NOT_FOUND",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status INTERNAL_SERVER_ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    })
     @GetMapping("/{taskId}")
     public ResponseEntity<TaskDTO> getTaskById(@PathVariable("taskId") Long taskId) {
 
@@ -41,6 +100,23 @@ public class TaskController {
                 .body(response);
     }
 
+    @Operation(
+            summary = "Get list of all Tasks Details REST API",
+            description = "REST API to get list of all tasks information"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status INTERNAL_SERVER_ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    })
     @GetMapping
     public ResponseEntity<List<TaskDTO>> getAllTTasks() {
 
@@ -51,6 +127,30 @@ public class TaskController {
                 .body(response);
     }
 
+    @Operation(
+            summary = "Update Task Details REST API",
+            description = "REST API for updating task information based on taskId"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP Status NOT_FOUND",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status INTERNAL_SERVER_ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    })
     @PutMapping("/{taskId}/update")
     public ResponseEntity<TaskDTO> updateUserByUsername(@Valid @RequestBody TaskDTO taskDTO,
                                                         @PathVariable("taskId") Long taskId) {
@@ -62,6 +162,30 @@ public class TaskController {
                 .body(response);
     }
 
+    @Operation(
+            summary = "Delete Task REST API",
+            description = "REST API for deleting task information based on taskId"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK"
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP Status NOT_FOUND",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "HTTP Status INTERNAL_SERVER_ERROR",
+                    content = @Content(
+                            schema = @Schema(implementation = ErrorResponseDTO.class)
+                    )
+            )
+    })
     @DeleteMapping("/{taskId}/delete")
     public ResponseEntity<ResponseDTO> deleteTaskById(@PathVariable("taskId") Long taskId) {
 
