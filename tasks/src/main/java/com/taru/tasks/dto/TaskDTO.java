899acdd27bba3a29619a4124927eeab0324edd8b
Timur Turianytsia <turianytsia.timur@gmail.com>
2024-02-09 21:06:14 +0200

    feat: added validation for Task create and update methods

--- tasks/src/main/java/com/taru/tasks/dto/TaskDTO.java
+++ tasks/src/main/java/com/taru/tasks/dto/TaskDTO.java
@@ -1,23 +1,41 @@
 package com.taru.tasks.dto;
 
-import lombok.Data;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class TaskDTO {
 
     private Long taskId;
 
+    @NotEmpty(message = "Username can not be a null or empty")
+    @Size(min = 5, max = 30, message = "The length of the username should be between 5 and 30")
     private String assignedTo;
 
+    @NotEmpty(message = "Title can not be a null or empty")
+    @Size(min = 5, max = 100, message = "The length of the title should be between 5 and 100")
     private String title;
 
+    @NotEmpty(message = "Description can not be a null or empty")
+    @Size(min = 5, max = 256, message = "The length of the description should be between 5 and 256")
     private String description;
 
+    @Future(message = "Due date must be in the future")
+    @NonNull
     private LocalDateTime dueDate;
 
+    @Positive(message = "Priority must be greater than zero")
     private Integer priority;
 
+    @NotEmpty(message = "Status can not be a null or empty")
+    @Size(min = 3, max = 20, message = "The length of the status should be between 3 and 20")
     private String status;
 }
